6470a78ff48ddd5eb63d4045eff05a62bfe6ce39
Yuna_H <ksm13563661@gmail.com>
2026-01-21 13:24:06 +0900

    주석 추가

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -4,17 +4,24 @@
 using namespace std;
 
 int solution(int n) {
+    // n=1이면 answer=2, n=4이면 answer=9가 나온다.
+    // answer에 i와 n/i를 한번에 더하면 (1, 1), (1, 2, 2, 4)로 중복된 값이 합해진다.
+    // set을 사용해서 중복된 값을 없앤다.
     set<int> s;
+    set<int>::iterator iter;
+
+    int answer = 0;
+    
     for(int i=1;i<=sqrt(n);i++) {
         if(n%i==0) {
             s.insert(i);
             s.insert(n/i);
         }
     }
-    set<int>::iterator iter;
-    int answer = 0;
+    
     for(iter=s.begin();iter!=s.end();iter++) {
         answer += (*iter);
     }
+    
     return answer;
 }
