4d2ba70d7c852eaebc56adeb4002e04cec99a93b
Yuna_H <ksm13563661@gmail.com>
2026-01-29 23:48:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int tmp = x, sum = 0;
+    while(tmp>0) {
+        sum += (tmp%10);
+        tmp/=10;
+    }
+    return x%sum == 0;
+}
