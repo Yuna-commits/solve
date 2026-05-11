102f7355904b5b9d7fecefadedd2839913c7f8b3
Yuna_H <ksm13563661@gmail.com>
2026-05-11 16:23:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -7,11 +7,7 @@ int solution(vector<int> absolutes, vector<bool> signs) {
     int answer = 0;
     
     for(int i = 0; i < absolutes.size(); i++) {
-        if (signs[i]) {
-            answer += absolutes[i];
-        } else {
-            answer -= absolutes[i];
-        }
+        answer += absolutes[i] * (signs[i] ? 1 : -1);
     }
     
     return answer;
