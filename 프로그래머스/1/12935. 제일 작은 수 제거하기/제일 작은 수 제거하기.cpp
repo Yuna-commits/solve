712957d5b7fb2d4d6bce2576589fe296e0861a20
Yuna_H <ksm13563661@gmail.com>
2026-07-29 23:28:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 9.76 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -1,14 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    int index = min_element(arr.begin(), arr.end()) - arr.begin();
-    arr.erase(arr.begin() + index);
-    if(arr.size() == 0) {
-        arr.push_back(-1);
-    }
-    return arr;
+    vector<int> answer;
+    return answer;
 }
