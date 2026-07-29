2eaec3823d3bfb28de25ba2a866f370ef5a5bcae
Yuna_H <ksm13563661@gmail.com>
2026-07-30 00:09:36 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -1,11 +1,10 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 string solution(string phone_number) {
-    for(auto i = 0; i<phone_number.length() - 4; i++) {
+    for(int i = 0; i<phone_number.length() - 4; i++) {
         phone_number.replace(i, 1, "*");
     }
     return phone_number;
