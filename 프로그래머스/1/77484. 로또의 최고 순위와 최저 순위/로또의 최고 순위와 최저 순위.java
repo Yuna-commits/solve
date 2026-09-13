d5917667bfcc6f17530d72adbe70a5c105c54392
Yuna_H <ksm13563661@gmail.com>
2026-09-13 21:37:41 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 11.15 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,34 +1,13 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.stream.LongStream;
 
 class Solution {
-    private int getRank(int match) {
-        switch(match) {
-            case 6: return 1;
-            case 5: return 2;
-            case 4: return 3;
-            case 3: return 4;
-            case 2: return 5;
-            default: return 6;
-        }
-    }
-    
-    public int[] solution(int[] lottos, int[] win_nums) {
-        int match = 0;
-        
-        List<Integer> lotto = Arrays.stream(lottos).boxed().toList();
-        List<Integer> win = Arrays.stream(win_nums).boxed().toList();
-        
-        int zero = Collections.frequency(lotto, 0);
-        
-        for(int num : lotto) {
-            if(win.contains(num)) {
-                match++;
-            }
-        }
-        
-        int max = getRank(match + zero);
-        int min = getRank(match);
-        
-        return new int[] {max, min};
+    public int[] solution(int[] lottos, int[] winNums) {
+        return LongStream.of(
+                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l) || l == 0).count(),
+                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l)).count()
+        )
+                .mapToInt(op -> (int) (op > 6 ? op - 1 : op))
+                .toArray();
     }
 }
