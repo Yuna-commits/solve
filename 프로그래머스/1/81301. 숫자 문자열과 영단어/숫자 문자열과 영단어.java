217db5dde77069df94e25fae79b961c93f629818
Yuna_H <ksm13563661@gmail.com>
2026-09-18 21:45:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.29 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,36 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        Map<String, String> map = Map.ofEntries(
-            Map.entry("zero", "0"),
-            Map.entry("one", "1"),
-            Map.entry("two", "2"),
-            Map.entry("three", "3"),
-            Map.entry("four", "4"),
-            Map.entry("five", "5"),
-            Map.entry("six", "6"),
-            Map.entry("seven", "7"),
-            Map.entry("eight", "8"),
-            Map.entry("nine", "9")
-        );
-        StringBuilder result = new StringBuilder();
-        StringBuilder tmp = new StringBuilder();
+       String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        for(char ch : s.toCharArray()) {
-            // 숫자인 경우
-            if(Character.isDigit(ch)) {
-                result.append(ch);
-            } 
-            // 문자인 경우
-            else {
-                tmp.append(ch);
-                if(map.containsKey(tmp.toString())) {
-                    result.append(map.get(tmp.toString()));
-                    tmp.setLength(0);
-                }
-            }
+        for(int i=0; i<words.length; i++) {
+            s = s.replaceAll(words[i], Integer.toString(i));
         }
         
-        return Integer.parseInt(result.toString());
+        return Integer.parseInt(s);
     }
 }
